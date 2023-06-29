53f558d30be4a5e71dd1b998aba2bb384d4f1a64
sodenn <mail@sodenn.dev>
2023-06-29 21:37:24 +0200

    docs(): update theme classes

--- apps/docs/app/theme.ts
+++ apps/docs/app/theme.ts
@@ -1,18 +1,19 @@
 const mentionsStyle =
   "px-1 mx-px align-baseline inline-block rounded break-words cursor-pointer select-none leading-5";
-const mentionsStyleFocused = "outline-none shadow-md shadow-gray-900";
+const mentionsStyleFocused =
+  "outline-none shadow-md shadow-gray-500 dark:shadow-gray-900";
 
 const theme = {
   ltr: "text-left",
   rtl: "text-right",
   beautifulMentions: {
-    "@": `${mentionsStyle} bg-green-500 text-gray-950`,
+    "@": `${mentionsStyle} dark:bg-green-500 bg-green-600 dark:text-gray-950 text-white`,
     "@Focused": mentionsStyleFocused,
-    "#": `${mentionsStyle} bg-blue-400 text-gray-950`,
+    "#": `${mentionsStyle} dark:bg-blue-400 bg-blue-600 dark:text-gray-950 text-white`,
     "#Focused": mentionsStyleFocused,
-    "due:": `${mentionsStyle} bg-yellow-400 text-gray-950`,
+    "due:": `${mentionsStyle} dark:bg-yellow-400 bg-yellow-600 dark:text-gray-950 text-white`,
     "due:Focused": mentionsStyleFocused,
-    "\\w+:": `${mentionsStyle} bg-gray-400 text-gray-950`,
+    "\\w+:": `${mentionsStyle} dark:bg-gray-400 bg-gray-500 dark:text-gray-950 text-white`,
     "\\w+:Focused": mentionsStyleFocused,
   },
 };
