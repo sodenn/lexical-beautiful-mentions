8fc9dd10ab2ed6baf263d268ff656a7b2ac8f84a
sodenn <mail@sodenn.dev>
2023-07-23 12:06:20 +0200

    combobox styling adjustments

--- apps/docs/app/ComboboxItem.tsx
+++ apps/docs/app/ComboboxItem.tsx
@@ -11,7 +11,7 @@ const ComboboxItem = forwardRef<
     className={cn(
       "mx-1.5 flex min-w-[150px] shrink-0 cursor-pointer flex-row content-center whitespace-nowrap rounded border-0 px-2.5 py-1.5 leading-4 text-slate-950 outline-none last:mb-1 last:scroll-mb-1 dark:text-slate-300",
       selected
-        ? "bg-gray-100 dark:bg-gray-700"
+        ? "bg-gray-200 dark:bg-gray-700"
         : "bg-slate-300 dark:bg-slate-600",
     )}
     {...props}
