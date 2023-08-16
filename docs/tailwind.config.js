922e507bf4d6679812775ab6fc7fd709094c4d98
sodenn <mail@sodenn.dev>
2023-08-16 20:40:30 +0200

    Update Tailwind CSS configuration

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
+    "./lib/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
