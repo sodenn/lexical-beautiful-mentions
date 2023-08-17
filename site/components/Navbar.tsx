17b81ffc474f36c8e8b2852028d344d8499f7f66
sodenn <mail@sodenn.dev>
2023-08-17 18:30:33 +0200

    add GithubButton

--- site/components/Navbar.tsx
+++ site/components/Navbar.tsx
@@ -1,24 +1,17 @@
+import { GithubButton } from "@/components/GithubButton";
 import { ModeToggle } from "@/components/ModeToggle";
 
 export function Navbar() {
   return (
     <div className="flex py-2">
       <div className="container flex justify-between gap-2">
-        <div className="flex flex-col">
-          <h1 className="inline-block text-2xl font-bold tracking-tight">
-            lexical-beautiful-mentions
-          </h1>
-          <div>
-            <a
-              className="text-primary underline"
-              href="https://github.com/sodenn/lexical-beautiful-mentions"
-              target="_blank"
-            >
-              GitHub
-            </a>
-          </div>
+        <h1 className="inline-block text-2xl font-bold tracking-tight">
+          lexical-beautiful-mentions
+        </h1>
+        <div className="flex gap-2">
+          <GithubButton />
+          <ModeToggle />
         </div>
-        <ModeToggle />
       </div>
     </div>
   );
