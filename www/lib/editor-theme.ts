7163ebcb139fb7dac72b259c4d63dbe48f17a54d
sodenn <mail@sodenn.dev>
2023-12-27 19:11:59 +0100

    Update editor theme

--- www/lib/editor-theme.ts
+++ www/lib/editor-theme.ts
@@ -2,7 +2,7 @@ import { BeautifulMentionsTheme } from "lexical-beautiful-mentions";
 
 const mentionsStyle =
   "px-1 mx-2/3 mx-px align-baseline inline-block rounded break-words cursor-pointer select-none leading-5";
-const mentionsStyleFocused = "ring-offset ring-2 ring-ring ring-offset-1";
+const mentionsStyleFocused = "outline ring-2 ring-offset-1";
 
 const beautifulMentionsTheme: BeautifulMentionsTheme = {
   "@": `${mentionsStyle} dark:bg-green-500 bg-green-600 text-accent`,
