59e358393cabd56ab4fe4134a6e3239089cb0528
商海涛 <shanghaitao@ydjdev.com>
2024-10-24 08:57:49 +0800

    fix(): add useEffect deps

--- plugin/src/BeautifulMentionsPlugin.tsx
+++ plugin/src/BeautifulMentionsPlugin.tsx
@@ -680,6 +680,7 @@ export function BeautifulMentionsPlugin(props: BeautifulMentionsPluginProps) {
     editor,
     triggers,
     punctuation,
+    autoSpace,
     allowSpaces,
     insertOnBlur,
     creatable,
