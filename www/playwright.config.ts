d953835ccde6e8b8bca5a2be8d3954f50c9aee25
sodenn <mail@sodenn.dev>
2025-03-25 22:24:03 +0100

    enable tracing

--- www/playwright.config.ts
+++ www/playwright.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    // trace: "on-first-retry",
+    trace: "on",
   },
 
   /* Configure projects for major browsers */
