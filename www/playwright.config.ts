6398786f70c3f5b87c946343decff40e4d7fdb6b
sodenn <mail@sodenn.dev>
2025-03-25 22:28:59 +0100

    configuration to retain traces on failure

--- www/playwright.config.ts
+++ www/playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     // trace: "on-first-retry",
-    trace: "on",
+    trace: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
