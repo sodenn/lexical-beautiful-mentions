79d98318a5fd680a84e728ec0e006910f605a688
sodenn <mail@sodenn.dev>
2025-03-25 22:32:41 +0100

    enable retries for CI in Playwright configuration

--- www/playwright.config.ts
+++ www/playwright.config.ts
@@ -16,8 +16,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  // retries: process.env.CI ? 1 : 0,
-  retries: 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -28,7 +27,6 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    // trace: "on-first-retry",
     trace: "retain-on-failure",
   },
 
