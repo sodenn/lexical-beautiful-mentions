1cb250c1208d649ac030c32455b24c28c524a06f
Victor Lineros <victorlineros@gmail.com>
2025-08-17 04:58:22 -0600

    fix(createMentionNode): Add 'withKlass' to lexical node replacement (#1008)
    * added withKlass to lexical node replacement
    
    * add changeset
    
    ---------
    
    Co-authored-by: Victor Lineros <VLineros@armlend.com>
    Co-authored-by: Dennis Soehnen <mail@sodenn.dev>

---
+++ .changeset/swift-points-clap.md
@@ -0,0 +1,5 @@
+---
+"lexical-beautiful-mentions": patch
+---
+
+fix(createMentionNode): Add 'withKlass' to lexical node replacement
--- plugin/src/createMentionNode.ts
+++ plugin/src/createMentionNode.ts
@@ -38,6 +38,7 @@ export function createBeautifulMentionNode(
           node.getData(),
         );
       },
+      withKlass: CustomBeautifulMentionNode
     },
   ];
 }
